d09cefb504fa98adf8e34545bc74ea71a17d712c
xiamingjie <15273155904@163.com>
2022-01-19 12:04:26 +0800

    添加测试代码和文件

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+//
+// Created by xiamingjie on 2022/1/19.
+//
+
+#include <fstream>
+#include <iostream>
+#include "iniconfig.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input config file." << endl;
+        return 1;
+    }
+
+    string file(argv[1]);
+    fstream input(file, ios::in);
+
+    Ini::IniConfig iniConfig;
+
+    try {
+        //解析
+        iniConfig.parse(input);
+    } catch (const ios_base::failure& e1) {
+        cerr << e1.what() << endl;
+    } catch (const Ini::ErrorLog& e2) {
+        cerr << e2.what() << endl;
+    }
+
+    cout << "遍历查找: " << endl;
+    //遍历
+    for (int i = 0; i < iniConfig.size(); i++) {
+        try {
+            auto parameter = iniConfig[i];
+            for (const auto &p: parameter) {
+                cout << p.first << "=" << p.second << endl;
+            }
+        } catch (const out_of_range& e) {
+            cerr << e.what() << endl;
+        }
+    }
+
+    cout << endl << "键值对查找: " << endl;
+
+    //键值查找
+    Ini::Parameters mysql = iniConfig["MySQL"];
+    string host = mysql["host"];
+    string port = mysql["port"];
+    string user = mysql["user"];
+    string pwd = mysql["password"];
+
+    cout << "主机: " << host << endl;
+    cout << "端口: " << port << endl;
+    cout << "用户名: " << user << endl;
+    cout << "密码: " << pwd << endl;
+
+    return 0;
+}
